ae812422c299ace2a91d9d4cd6aa996bfb9b1c26
Antoni <antoni.jakowicz7@gmail.com>
2024-04-18 23:07:03 +0200

    v1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import Card from "./Card";
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import './index.css'
 
 function App() {
 
-
-
- const fetchData = ()=> {}
-
-  const headers = new Headers({
-    "Content-Type": "application/json",
-    "x-api-key": "live_dHCl88eFrm023zFcznv2DWQvf25ae1HLqFrTGPSx8aQMpV8cfXdbCgWj7UPzPyeg"
-  });
   
-  var requestOptions = {
-    method: 'GET',
-    headers: headers,
-    redirect: 'follow'
-  };
-  
-  fetch("https://api.thedogapi.com/v1/images/search?limit=10", requestOptions)
-    .then(response => response.text())
-    .then( data => console.log(data))
-    .catch(error => console.log('error', error)); 
 
-    //.then(result => console.log(result))
+  const [dogs, setDogs] = useState([])
+
+
+  const fetchData = ()=>{
+    fetch("https://api.thedogapi.com/v1/breeds")
+       .then( res => res.json())
+       .then( data =>  setDogs(data) )
+       .catch( err => console.log(err.message))
+  }
+
+useEffect(fetchData, [])
+
   return (
     <>
-      <h1>Pieski</h1>
-      
+      <section>
+        <h1 className='main-text'>The Dog App</h1>
+
+        <div className='dog-list'>
+          {dogs.map((dog)=>(
+            <div className="card" key={dog.id}>
+              <img src={`https://cdn2.thedogapi.com/images/${dog.reference_image_id}.jpg`}  className='dog-photo'></img>
+              <h3>{dog.name}</h3>
+              <p>Weight: {dog.weight.metric} kg</p>
+              <p>Height: {dog.height.metric} cm</p>
+              <p>Life span: {dog.life_span}</p>
+            </div>
+          ))}
+        </div>
+      </section>
     </>
   )
 }
